6a83b96e6e65c83a163f4bd10d83b0c132097c38
112tenday <iwangunawantondang@gmail.com>
2024-02-07 21:14:33 +0700

    Add Controller for getall,getbyId,save,and delete operations

---
+++ src/main/java/com/iwan/crud/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.iwan.crud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iwan.crud.model.User;
+import com.iwan.crud.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userid}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userid) {
+        User user = userService.getUserById(userid);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @DeleteMapping("/delete/{userid}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userid) {
+        userService.deleteUser(userid);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+}
