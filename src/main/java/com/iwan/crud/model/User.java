5964809a28b6fadbac31e8dc89fe1e031cc11fa7
112tenday <iwangunawantondang@gmail.com>
2024-02-07 21:09:14 +0700

    feat: Add User model with JPA annotations

---
+++ src/main/java/com/iwan/crud/model/User.java
@@ -0,0 +1,55 @@
+package com.iwan.crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+    
+    private String namalengkap;
+    private String username;
+    private String password;
+    private String status;
+
+    public Long getUserid() {
+        return userid;
+    }
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+    public String getNamalengkap() {
+        return namalengkap;
+    }
+    public void setNamalengkap(String namalengkap) {
+        this.namalengkap = namalengkap;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+    
+}
